29a31cd87e6b1b3498245eead42b6475e3bb58ad
siddarthg <siddarthg@nvidia.com>
2024-07-20 03:12:11 -0700

    Modified pluginw

--- plugin_install.sh
+++ plugin_install.sh
@@ -9,7 +9,12 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Clone zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zshrc_plugin/zsh-syntax-highlighting
+echo "###################################################  zsh-syntax-highlighting search installation completed successfully!################################################### "
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/zshrc_plugin/zsh-history-substring-search 
+echo " ################################################### zsh-substring search installation completed successfully!################################################### "
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/zshrc_plugin/zsh-autosuggestions
+echo "################################################### zsh-autosuggestions installation completed successfully!################################################### "
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git  ~/zshrc_plugin/powerlevel10k
+echo "###################################################  Powerlevel10k installation completed successfully!################################################### "
 
 echo "Installation completed successfully!"
